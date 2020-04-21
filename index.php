2438ab7b570f6a885173b5aa398712d0b706bce7
yousefghanem <49624768+yousefghanem@users.noreply.github.com>
2020-04-21 12:22:57 -0700

    Uploaded all the files

---
+++ index.php
@@ -0,0 +1,16 @@
+ <html>
+  <head>
+	<link href="Bootstrap_4.3.1/css/bootstrap.css" type='text/css' rel="stylesheet"> </head>
+
+    <title> PHP Test</title>
+    </head>
+    <?php echo '<p>Hello World! Here are all my links [Yousef Ahmed, Period 7]</p>'; ?>
+    <li><a href="index.html">index.html</a></li></br>
+    <li><a href="Last_AboutMe.html">About Me</a></li></br>
+				<li><a href="Last_Resume.html"> Resume </a></li></br>
+				<li><a href="Last_ContactMe.html"> Contact me</a>
+				</li></br>
+				<br><li><a href="Last_Projects"> Last_Projects </a></li></br>                   
+        </header>
+  </body>
+</html>
